6d779d47550150a8d6b0b68fb501508d0d6f34d9
garry-p12 <105554007+garry-p12@users.noreply.github.com>
2022-08-03 22:56:26 +0530

    Add files via upload

---
+++ Satisfaction/app.py
@@ -0,0 +1,20 @@
+#import libraries
+import numpy as np
+from flask import Flask, render_template,request
+import pickle#Initialize the flask App
+app = Flask(__name__)
+model = pickle.load(open('employee_satisfaction_1.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+#To use the predict button in our web-app
+@app.route('/predict',methods=['POST'])
+def predict():
+    #For rendering results on HTML GUI
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2) 
+    return render_template('home.html', prediction_text='Predicted status of the employee is :{}'.format(output))
+if __name__ == "__main__":
+    app.run(debug=True)
